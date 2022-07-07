e9f29c2549a209c69d7960b86a7331debecfa6a7
Tatiana Simmer <tatkagore@gmail.com>
2022-07-07 16:38:13 +0200

    Initial Commit

---
+++ Shared/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
